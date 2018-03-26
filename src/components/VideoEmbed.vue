cd64292799b32db7ed7b067b7f648933c632fccc
JB <jb@visualjerk.de>
2018-03-26 22:59:41 +0200

    handle videos

--- src/components/VideoEmbed.vue
+++ src/components/VideoEmbed.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="ql-video-embed">
-    <div v-if="meta.embed.html" v-html="meta.embed.html"></div>
+    <iframe :src="embedUrl"
+      frameborder="0"
+      webkitallowfullscreen
+      mozallowfullscreen
+      allowfullscreen></iframe>
   </div>
 </template>
 
@@ -18,9 +22,37 @@
           return {}
         }
       }
+    },
+    computed: {
+      embedUrl () {
+        if (this.meta.site_name === 'Vimeo') {
+          return this.meta.player.url
+        }
+        if (this.meta.site_name === 'YouTube') {
+          const videoUrl = this.meta.url.split('watch?v=')[1]
+          return `https://www.youtube.com/embed/${videoUrl}?feature=oembed`
+        }
+        return ''
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .ql-video-embed {
+    position: relative;
+    width: 100%;
+    padding-top: 56%;
+
+    iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      border: 0;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
